9731bb18423bfc4c9672aa87aded2d7821a88544
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2010-03-23 21:10:13 +0000

    Reorganized premake4.lua files into subdirectories.

---
+++ lua/wxlua/premake4.lua
@@ -0,0 +1,28 @@
+project "wxlua"
+    -- --------------------------------------------------------------------
+    -- General
+    -- --------------------------------------------------------------------
+    kind "StaticLib"
+    language "C++"
+    files { "src/*", "include/*" }
+    includedirs { "../wxbind/setup", "../lua/include", "..",  }
+    links { "lua" }
+
+    configuration "Debug"   targetdir "../../lib/Debug"
+    configuration "Release" targetdir "../../lib/Release"
+
+    -- --------------------------------------------------------------------
+    -- Platform-specific
+    -- --------------------------------------------------------------------
+    configuration "windows"
+        defines { "_LIB" }
+
+    -- Disable some warnings
+    configuration "vs*"
+        buildoptions {
+            "/wd4800", -- implicit conversion to bool
+            "/wd4390", -- empty control statement
+        }
+
+    -- wxWidgets
+    dofile "../../premake4_wxdefs.lua"
