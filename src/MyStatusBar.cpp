2823eca95f5a0a6ef903c75db0ec718262fca42d
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-06-07 06:51:59 +0000

    Status bar displays lua errors Misc status bar functions

--- src/MyStatusBar.cpp
+++ src/MyStatusBar.cpp
@@ -1,5 +1,5 @@
 // This file is part of XWord
-// Copyright (C) 2009 Mike Richards ( mrichards42@gmx.com )
+// Copyright (C) 2011 Mike Richards ( mrichards42@gmx.com )
 //
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -22,8 +22,23 @@
 #include "widgets/SizedText.hpp"
 #include "messages.hpp"
 
+static enum StatusIds
+{
+    STATUS_FIRST = 0,
+    STATUS_GENERAL = STATUS_FIRST,
+#ifdef XWORD_USE_LUA
+    STATUS_LUA,
+#endif
+    STATUS_ALERT,
+    STATUS_TIME,
+    STATUS_LAST,
+
+    STATUS_TOTAL = STATUS_LAST - STATUS_FIRST
+};
+
 BEGIN_EVENT_TABLE(MyStatusBar, wxStatusBar)
-    EVT_SIZE(MyStatusBar::OnSize)
+    EVT_SIZE        (        MyStatusBar::OnSize )
+    EVT_LEFT_DCLICK ( MyStatusBar::OnDoubleClick )
 END_EVENT_TABLE()
 
 MyStatusBar::MyStatusBar(wxWindow * parent,
@@ -32,8 +47,14 @@ MyStatusBar::MyStatusBar(wxWindow * parent,
                          const wxString & name)
     : wxStatusBar(parent, id, style, name)
 {
-    int widths[] = {-2, -1, 200};
-    SetFieldsCount(3, widths);
+#ifdef XWORD_USE_LUA
+    int widths[] = {-2, 150, -2, 100};
+#else
+    int widths[] = {-2, -1, 100};
+#endif // XWORD_USE_LUA
+
+    SetFieldsCount(STATUS_TOTAL, widths);
+
     m_alert = new SizedText(this, wxID_ANY);
     m_alert->SetAlign(wxALIGN_CENTER);
     m_alert->SetWrapMode(ST_TRUNCATE);
@@ -45,19 +66,25 @@ MyStatusBar::MyStatusBar(wxWindow * parent,
                  NULL, this);
 }
 
-void
-MyStatusBar::OnSize(wxSizeEvent & WXUNUSED(evt))
+void MyStatusBar::SetStatus(const wxString & text)
 {
-    wxRect rect;
-    if (GetFieldRect(1, rect)) {
-        m_alert->SetSize(rect);
-        //WrapAlert(rect.width);
-    }
+    SetStatusText(text, STATUS_GENERAL);
 }
 
+#ifdef XWORD_USE_LUA
+void MyStatusBar::SetLuaErrors(int num)
+{
+    if (num < 1)
+        SetStatusText(_T(""), STATUS_LUA);
+    else if (num == 1)
+        SetStatusText(_T("1 Error"), STATUS_LUA);
+    else
+        SetStatusText(wxString::Format(_T("%d Errors"), num), STATUS_LUA);
+}
+#endif // XWORD_USE_LUA
 
-void
-MyStatusBar::SetAlert(const wxString & text, const wxColour & fgColor, const wxColour & bgColor)
+
+void MyStatusBar::SetAlert(const wxString & text, const wxColour & fgColor, const wxColour & bgColor)
 {
     m_alert->SetBackgroundColour(bgColor);
     m_alert->SetForegroundColour(fgColor);
@@ -66,12 +93,32 @@ MyStatusBar::SetAlert(const wxString & text, const wxColour & fgColor, const wxC
     //WrapAlert();
 }
 
+
+void MyStatusBar::SetTime(int time)
+{
+    SetStatusText( wxTimeSpan::Seconds(time).Format(), STATUS_TIME);
+}
+
+
+
+
+void
+MyStatusBar::OnSize(wxSizeEvent & WXUNUSED(evt))
+{
+    wxRect rect;
+    if (GetFieldRect(STATUS_ALERT, rect)) {
+        m_alert->SetSize(rect);
+        //WrapAlert(rect.width);
+    }
+}
+
+
 void
 MyStatusBar::OnAlertClick(wxMouseEvent & evt)
 {
     wxWindow * alert = wxDynamicCast(evt.GetEventObject(), wxWindow);
     if (alert)
-        XWordMessage(alert->GetToolTip()->GetTip());
+        XWordMessage(this, alert->GetToolTip()->GetTip());
 }
 
 void
@@ -79,10 +126,56 @@ MyStatusBar::WrapAlert(int width)
 {
     if (width == -1) {
         wxRect rect;
-        GetFieldRect(1, rect);
+        GetFieldRect(STATUS_ALERT, rect);
         width = rect.width;
     }
 
     wxString wrapped = Wrap(m_alert, m_alert->GetToolTip()->GetTip(), width, NULL);
     m_alert->SetLabel(wrapped.BeforeFirst(_T('\n')));
+}
+
+
+#ifdef XWORD_USE_LUA
+#include "paths.hpp"
+
+class LuaErrorsDlg : public wxDialog
+{
+public:
+    LuaErrorsDlg(wxWindow * parent, const wxString & filename)
+        : wxDialog(parent, wxID_ANY, _T("Errors"), wxDefaultPosition, wxDefaultSize, wxDEFAULT_DIALOG_STYLE | wxRESIZE_BORDER)
+    {
+        m_text = new wxTextCtrl(this, wxID_ANY, wxEmptyString,
+                                wxDefaultPosition, wxDefaultSize,
+                                wxTE_READONLY | wxTE_MULTILINE);
+        m_text->LoadFile(filename);
+
+        wxSizer * sizer = new wxBoxSizer(wxVERTICAL);
+        sizer->Add(m_text, 1, wxALL | wxEXPAND, 10);
+        sizer->Add(CreateButtonSizer(wxOK), 0, wxALL |wxEXPAND, 10);
+        SetSizer(sizer);
+    }
+
+private:
+    wxTextCtrl * m_text;
+};
+
+#endif // XWORD_USE_LUA
+
+void MyStatusBar::OnDoubleClick(wxMouseEvent & evt)
+{
+    wxRect rect;
+#ifdef XWORD_USE_LUA
+    if (! GetFieldRect(STATUS_LUA, rect))
+        return;
+
+    if (rect.Contains(evt.GetPosition()))
+    {
+        wxWindow * errors = wxDynamicCast(evt.GetEventObject(), wxWindow);
+        if (errors)
+        {
+            LuaErrorsDlg dlg(this, GetLuaLogFilename());
+            dlg.ShowModal();
+        }
+    }
+#endif // XWORD_USE_LUA
 }
