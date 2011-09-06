58e9e0acc1b9d85296dc2f1889fbb78aa5a77869
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-09-06 19:03:38 +0000

    Updated swap across and down script to work with changes to luapuz

--- trunk/scripts/swap/info.lua
+++ trunk/scripts/swap/info.lua
@@ -1,6 +1,7 @@
 return {
     name = "Swap across and down",
+    packagename = "swap",
     version = "1.0",
     description = "Swap across and down words and clues in the grid for a new perspective.",
-    requires = "0.4"
+    requires = "0.5"
 }
