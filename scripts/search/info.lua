6c3078c1b8724aff7374b9f461bd220878a16235
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-09-06 19:08:41 +0000

    Added packagename field to info.lua in nextblank and search packages.

--- scripts/search/info.lua
+++ scripts/search/info.lua
@@ -1,10 +1,11 @@
 return {
     name = "Search",
+    packagename = "search",
     version = "1.0",
     description = [[
 Adds a search menu with the following entries:
     * OneAcross - search for the current clue and word
     * Wikipedia - search for the current word
 ]],
-    requires = "0.4"
+    requires = "0.5"
 }
