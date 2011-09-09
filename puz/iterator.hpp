250c1305b578ddc7a9459310160ce36895234f78
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-09-09 02:50:37 +0000

    const_square_iterators allowed to be converted to square_iterators

--- puz/iterator.hpp
+++ puz/iterator.hpp
@@ -19,89 +19,111 @@
 #define PUZ_ITERATOR_H
 
 #include "Square.hpp"
-#include "memory"
+#include <memory>
 
 namespace puz {
+
+// There is a basic deficiency in this iterator implementation in that
+// const iterators aren't really possible.
+// Storing a pointer to iterator_impl makes it so that Square is always
+// returned non-const.  I don't *really* think that is a problem given that
+// we don't ever use a const Grid (where the Squares are actually stored).
+
+// Really this just opens up the big can of worms that is the entire
+// design of const-correctness in the puz library.
 
 // base class for iterator implementation
 class iterator_impl
 {
 public:
-    iterator_impl() {}
     virtual iterator_impl * clone()=0;
+    virtual ~iterator_impl() {}
     virtual void increment()=0;
     virtual void decrement()=0;
     virtual Square * ptr()=0;
     virtual Square & ref()=0;
 };
 
+// A null iterator
+class null_iterator_impl : public iterator_impl
+{
+public:
+    null_iterator_impl() {}
+    null_iterator_impl * clone() { return new null_iterator_impl(); }
+    void increment() {}
+    void decrement() {}
+    Square * ptr() { return NULL; }
+    Square & ref() { throw std::exception(); }
+};
+
 // ----------------------------------------------------------------------------
-// The square iterator
+// The square iterator (wrapper for implementation classes)
 // ----------------------------------------------------------------------------
 template <typename Square_T, bool INC = true>
     class square_iterator_t
 {
 public:
     typedef square_iterator_t<Square_T, INC> self_t;
+    // This allows conversion between square_iterator and const_square_iterator
+    friend class square_iterator_t<const Square, INC>;
 
-    explicit square_iterator_t<Square_T, INC>(iterator_impl * it = NULL)
+
+    square_iterator_t<Square_T, INC>()
+        : m_impl(new null_iterator_impl())
+    {}
+
+    explicit square_iterator_t<Square_T, INC>(iterator_impl * it)
         : m_impl(it)
     {}
 
     // Copy constructor: make a copy of m_impl
-    square_iterator_t<Square_T, INC>(const self_t & other)
-        : m_impl(other.m_impl.get() ? other.m_impl->clone() : NULL)
+    template <typename OTHER>
+    square_iterator_t<Square_T, INC>(const OTHER & other)
+        : m_impl(other.m_impl->clone())
     {}
 
     // Assignment
-    self_t & operator=(const self_t & other)
+    template <typename OTHER>
+    self_t & operator=(const OTHER & other)
     {
-        m_impl.reset(other.m_impl.get() ? other.m_impl->clone() : NULL);
+        m_impl.reset(other.m_impl->clone());
         return *this;
     }
 
     // Comparison
-    bool operator==(const self_t & other) const
+    template <typename OTHER>
+    bool operator==(const square_iterator_t<OTHER, INC> & other) const
     {
         return m_impl->ptr() == other.m_impl->ptr();
     }
 
-    bool operator!=(const self_t & other) const
+    template <typename OTHER>
+    bool operator!=(const square_iterator_t<OTHER, INC> & other) const
     {
         return m_impl->ptr() != other.m_impl->ptr();
     }
 
-    // Prefix operators
-    self_t & operator++()
+    bool operator==(const Square * other) const
     {
-        increment();
-        return *this;
+        return m_impl->ptr() == other;
     }
 
-    self_t & operator--()
+    bool operator!=(const Square * other) const
     {
-        decrement();
-        return *this;
+        return m_impl->ptr() != other;
     }
+
+    // Prefix operators
+    self_t & operator++() { increment(); return *this; }
+    self_t & operator--() { decrement(); return *this; }
 
     // Postfix operators
-    self_t operator++(int)
-    {
-        self_t it = *this;
-        increment();
-        return it;
-    }
-
-    self_t operator--(int)
-    {
-        self_t it = *this;
-        decrement();
-        return it;
-    }
+    self_t operator++(int) { self_t it = *this; increment(); return it; }
+    self_t operator--(int) { self_t it = *this; decrement(); return it; }
 
     // Pointer operators
-    Square_T & operator*() { return m_impl->ref(); }
-    Square_T * operator->() { return &*m_impl->ptr(); }
+    Square & operator*() const { return m_impl->ref(); }
+    Square * operator->() const { return &*m_impl->ptr(); }
 
 protected:
     std::auto_ptr<iterator_impl> m_impl;
@@ -132,114 +154,36 @@ void square_iterator_t<const Square, false>::decrement()
     m_impl->increment();
 }
 
+// Typedefs
 typedef square_iterator_t<Square, true> square_iterator;
 typedef square_iterator_t<const Square, true> const_square_iterator;
 typedef square_iterator_t<Square, false> square_reverse_iterator;
 typedef square_iterator_t<const Square, false> const_square_reverse_iterator;
-/*
-// ----------------------------------------------------------------------------
-// Reverse iterator
-// ----------------------------------------------------------------------------
 
-template <typename Square_T>
-    class square_reverse_iterator_t
-{
-public:
-    explicit square_reverse_iterator_t<Square_T>(iterator_impl * it = NULL) : m_impl(it) {}
-
-    // Copy constructor: make a copy of m_impl
-    square_reverse_iterator_t<Square_T>(const square_reverse_iterator_t<Square_T> & other)
-        : m_impl(other.m_impl.get() ? other.m_impl->clone() : NULL)
-    {}
-
-    // Assignment
-    square_reverse_iterator_t<Square_T> & operator=(const square_reverse_iterator_t<Square_T> other)
-    {
-        m_impl.reset(other.m_impl.get() ? other.m_impl->clone() : NULL);
-        return *this;
-    }
-
-    // Prefix operators
-    square_reverse_iterator_t<Square_T> & operator++()
-    {
-        m_impl->decrement();
-        return *this;
-    }
-
-    square_reverse_iterator_t<Square_T> & operator--()
-    {
-        m_impl->increment();
-        return *this;
-    }
-
-    // Postfix operators
-    square_reverse_iterator_t<Square_T> operator++(int)
-    {
-        square_reverse_iterator_t<Square_T> it = *this;
-        m_impl->decrement();
-        return it;
-    }
-
-    square_reverse_iterator_t<Square_T> operator--(int)
-    {
-        square_reverse_iterator_t<Square_T> it = *this;
-        m_impl->increment();
-        return it;
-    }
-
-    // Pointer operators
-    Square_T & operator*() { return m_impl->ref(); }
-    Square_T * operator->() { return &*m_impl->ptr(); }
-
-protected:
-    std::auto_ptr<iterator_impl> m_impl;
-};
-
-typedef square_reverse_iterator_t<Square> square_reverse_iterator;
-typedef square_reverse_iterator_t<const Square> const_square_reverse_iterator;
-*/
-
-// ----------------------------------------------------------------------------
-// Implementation classes
-// ----------------------------------------------------------------------------
-
-// Basic iterator
+// Basic iterator implementation
 // This is a drop-in replacement for the usual construction:
 // for (square = grid.First(); square != NULL; square = square->Next())
 // Instead you can use
 // for (square_iterator it = grid.begin(); it != grid.end(); ++it)
-class grid_iterator_impl : public iterator_impl
+class basic_iterator_impl : public iterator_impl
 {
 public:
-    grid_iterator_impl(Square * square = NULL) : m_square(square) {}
-
+    basic_iterator_impl(Square * square = NULL, GridDirection dir = ACROSS)
+        : m_square(square),
+          m_direction(dir)
+    {}
     virtual iterator_impl * clone()
-    {
-        return new grid_iterator_impl(m_square);
-    }
-
-    virtual void increment()
-    {
-        if (m_square == NULL)
-            throw std::out_of_range("Square out of range");
-        m_square = m_square->Next();
-    }
-
-    virtual void decrement()
-    {
-        if (m_square == NULL)
-            throw std::out_of_range("Square out of range");
-        m_square = m_square->Prev();
-    }
-
+        { return new basic_iterator_impl(m_square, m_direction); }
+    virtual void increment() { m_square = m_square->Next(m_direction); }
+    virtual void decrement() { m_square = m_square->Prev(m_direction); }
     virtual Square * ptr() { return m_square; }
     virtual Square & ref() { return *m_square; }
 
 protected:
     Square * m_square;
+    GridDirection m_direction;
 };
 
 } // namespace puz
 
 #endif // PUZ_ITERATOR_H
-
