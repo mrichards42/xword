7c3d61aaff6cc4e28df24a24c1c03ff47abb69aa
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-10-17 01:57:48 +0000

    Update search info.lua

--- scripts/search/info.lua
+++ scripts/search/info.lua
@@ -1,11 +1,14 @@
 return {
     name = "Search",
     packagename = "search",
-    version = "1.0",
+    version = "1.1",
     description = [[
 Adds a search menu with the following entries:
 * OneAcross - search for the current clue and word
 * Wikipedia - search for the current word
+
+Changes 10 version 1.1:
+* OneAcross search works with words that are all blank.
 ]],
     requires = "0.5"
 }
