b23021f6f083da23ef3429ef9d2846c79d16c022
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-10-17 02:08:09 +0000

    Updated import/info.lua

--- trunk/scripts/import/info.lua
+++ trunk/scripts/import/info.lua
@@ -1,10 +1,13 @@
 return {
     name = "Puzzle import",
     packagename = "import",
-    version = "1.0",
+    version = "1.1",
     description = [[
 Import non-native puzzle types:
 * UClick XML
+
+Changes for version 1.1 (XWord 0.5.3):
+* Cleaner, more detailed error messages.
 ]],
     requires = '0.5.3'
 }
