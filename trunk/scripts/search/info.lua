283dc5ed2e2f786ccc799d5e17256fb22ee15997
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2011-10-17 02:01:41 +0000

    Fixed typo in search/info.lua

--- trunk/scripts/search/info.lua
+++ trunk/scripts/search/info.lua
@@ -7,7 +7,7 @@ Adds a search menu with the following entries:
 * OneAcross - search for the current clue and word
 * Wikipedia - search for the current word
 
-Changes 10 version 1.1:
+Changes for version 1.1:
 * OneAcross search works with words that are all blank.
 ]],
     requires = "0.5"
