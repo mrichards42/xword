99a7720f04bab9d0e46894052f5cbb42816c16e8
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2012-01-20 02:53:50 +0000

    updated crosswordfiend blog

--- scripts/blog/crosswordfiend.lua
+++ scripts/blog/crosswordfiend.lua
@@ -1,10 +1,17 @@
+local function chop(str)
+    if str:sub(1,1) == "0" then
+        return str:sub(2)
+    else
+        return str
+    end
+end
+
 local function fiend(d)
     -- Day should not have a leading zero
-    local dayfmt = d:fmt("%d")
-    if dayfmt:sub(1,1) == "0" then
-        dayfmt = dayfmt:sub(2,2)
-    end
-    blog.open("http://www.crosswordfiend.com/blog/%A-%m" .. dayfmt .. "%y/", d)
+    local day = chop(d:fmt("%d"))
+    local month = chop(d:fmt("%m"))
+
+    blog.open("http://www.crosswordfiend.com/blog/%A-" .. month .. day .. "%y/", d)
 end
 
 blog.add_entry('Diary Of A Crossword Fiend', fiend)
