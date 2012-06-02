04b8777245fce07d2a646459b1e1d91ab5f4acd6
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2012-06-02 13:16:40 +0000

    Start branching off the GUI portion of download config into its own file

--- scripts/download/config.lua
+++ scripts/download/config.lua
@@ -277,431 +277,114 @@ local BmpButton = require 'download.bmp_button'
 local bmp = require 'download.bmp'
 
 
-function scrolled_panel(parent)
-    local scroller = wx.wxScrolledWindow(parent, wx.wxID_ANY)
-    scroller:SetWindowStyle(wx.wxBORDER_DOUBLE)
-    scroller:SetScrollRate(10, 10)
-
-    function scroller:update_scrollbars()
-        self.panel:Layout()
-        self:FitInside()
-        self:Refresh()
-    end
-
-    -- This panel preserves tab behavior
-    local psizer = wx.wxBoxSizer(wx.wxHORIZONTAL)
-    local panel = wx.wxPanel(scroller, wx.wxID_ANY)
-    psizer:Add(panel, 1, wx.wxEXPAND)
-    scroller:SetSizer(psizer)
-
-    scroller.panel = panel
-    return scroller
-end
-
-function collapse_panel(parent, label)
-    local panel = wx.wxPanel(parent, wx.wxID_ANY)
-    local sizer = wx.wxBoxSizer(wx.wxVERTICAL)
-    panel:SetSizer(sizer)
-
-    -- Header
-    local header = wx.wxBoxSizer(wx.wxHORIZONTAL)
-    sizer:Add(header, 0, wx.wxEXPAND + wx.wxBOTTOM, 5)
-
-    local arrow = ArrowButton(panel, wx.wxID_ANY)
-    header:Add(arrow, 0, wx.wxALIGN_CENTER_VERTICAL + wx.wxRIGHT, 5)
-    local text = TextButton(panel, wx.wxID_ANY, label)
-    header:Add(text, 0, wx.wxALIGN_CENTER_VERTICAL)
-
-    -- Collapsing
-    local inside_panel
-    local function update_panel()
-        if inside_panel then
-            sizer:Show(inside_panel, arrow:IsExpanded())
-            sizer:Layout()
-            parent.Parent:Fit()
-        end
-    end
-
-    arrow:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, update_panel)
-    
-    text:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function (evt)
-        arrow:Toggle()
-        update_panel()
-    end)
-
-    -- Public stuff
-    panel.header = header
-    panel.sizer = sizer
-    function panel:set_panel(p)
-        inside_panel = p
-        sizer:Add(inside_panel, 1, wx.wxEXPAND)
-        update_panel()
-    end
-
-    return panel
-end
-
 -- ----------------------------------------------------------------------------
 -- Advanced Options / New source dialog
 -- ----------------------------------------------------------------------------
-
-function get_curl_options_panel(parent, puzzle)
-    local panel = collapse_panel(parent, "Download Options")
-
-    -- Header additions
-    panel.header:AddStretchSpacer()
-    local link = TextButton(panel, wx.wxID_ANY, "(help)")
-    link:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function(evt)
-        wx.wxLaunchDefaultBrowser(
-            "http://curl.haxx.se/libcurl/c/curl_easy_setopt.html")
-    end)
-    link.ForegroundColour = wx.wxBLUE
-    panel.header:Add(link)
-
-    -- Options
-    local scroller = scrolled_panel(panel, wx.wxID_ANY)
-    panel:set_panel(scroller)
-
-    local grid = wx.wxFlexGridSizer(0, 3, 5, 5)
-    grid:AddGrowableCol(1)
-    scroller.panel:SetSizer(grid)
-
-    local curl_opt_list = {}
-    local cutl_opt_names = {}
-    for k, v in pairs(curl) do
-        if k:sub(1,4) == "OPT_" then
-            table.insert(curl_opt_list, k:sub(5))
-            curl_opt_names[v] = k:sub(5)
-        end
-    end
-    table.sort(curl_opt_list)
-
-    local opts = {}
-
-    local function add_option(id, value)
-        local opt = wx.wxChoice(
-            scroller.panel, wx.wxID_ANY,
-            wx.wxDefaultPosition, wx.wxDefaultSize, curl_opt_list)
-        opt.StringSelection = curl_opt_names[id] or ''
-        local text = wx.wxTextCtrl(scroller.panel, wx.wxID_ANY, value or '')
-        local remove_button = BmpButton(scroller.panel, wx.wxID_ANY, bmp.remove)
-        remove_button.ToolTip = wx.wxToolTip("Remove option")
-        grid:Insert(#opts * 3, opt)
-        grid:Insert(#opts * 3 + 1, text, 1, wx.wxEXPAND)
-        grid:Insert(#opts * 3 + 2, remove_button, 0, wx.wxALIGN_CENTER_VERTICAL)
-        table.insert(opts, {opt, text})
-
-        -- Remove event
-        remove_button:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function(evt)
-            for i, ctrls in ipairs(opts) do
-                if ctrls[1] == opt then
-                    table.remove(opts, i)
-                    grid:Detach((i-1) * 3)
-                    grid:Detach((i-1) * 3)
-                    grid:Detach((i-1) * 3)
-                    opt:Destroy()
-                    text:Destroy()
-                    remove_button:Destroy()
-                    scroller:update_scrollbars()
-                    break
-                end
-            end
-        end)
-    end
-
-    -- The add button
-    grid:AddSpacer(0)
-    grid:AddSpacer(0)
-    local add_button = BmpButton(scroller.panel, wx.wxID_ANY, bmp.add)
-    grid:Add(add_button, 0, wx.wxALIGN_CENTER_VERTICAL)
-    add_button.ToolTip = wx.wxToolTip("Add option")
-    add_button:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function (evt)
-        scroller:Freeze()
-        add_option()
-        scroller:update_scrollbars()
-        scroller:Thaw()
-    end)
-
-    scroller.MinSize = wx.wxSize(-1, 100)
-
-    function panel:apply()
-        if not puzzle.curlopts then
-            puzzle.curlopts = {}
-        end
-        tablex.clear(puzzle.curlopts)
-        for _, ctrls in ipairs(opts) do
-            puzzle.curlopts[curl['OPT_' .. ctrls[1].StringSelection]] = ctrls[2].Value
-        end
-        if #opts == 0 then
-            puzzle.curlopts = nil
-        end
-    end
-
-    function panel:update_puzzle(p)
-        -- Clear the grid sizer
-        for i=0,#opts*3-1 do
-            local window = grid:GetItem(0).Window
-            grid:Detach(0)
-            window:Destroy()
-        end
-        tablex.clear(opts)
-        for k, v in pairs(p.curlopts or {}) do
-            if type(v) == 'string' and curl_opt_names[k] then
-                add_option(k, v)
-            end
-        end
-        scroller:update_scrollbars()
-    end
-
-    panel:update_puzzle(puzzle)
-
-    return panel
-end
-
-function get_download_fields_panel(parent, puzzle)
-    local panel = collapse_panel(parent, "Custom User Options")
-
-    -- Fields
-    local scroller = scrolled_panel(panel, wx.wxID_ANY)
-    panel:set_panel(scroller)
-
-    local grid = wx.wxFlexGridSizer(0, 2, 5, 5)
-    grid:AddGrowableCol(0)
-    scroller.panel:SetSizer(grid)
-
-    local fields = {}
-
-    local function add_field(value)
-        local text = wx.wxTextCtrl(scroller.panel, wx.wxID_ANY, value or '')
-        local remove_button = BmpButton(scroller.panel, wx.wxID_ANY, bmp.remove)
-        remove_button.ToolTip = wx.wxToolTip("Remove field")
-        grid:Insert(#fields * 2, text, 1, wx.wxEXPAND)
-        grid:Insert(#fields * 2 + 1, remove_button, 0, wx.wxALIGN_CENTER_VERTICAL)
-        table.insert(fields, text)
-
-        -- Remove event
-        remove_button:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function(evt)
-            for i, ctrl in ipairs(fields) do
-                if ctrl == text then
-                    table.remove(fields, i)
-                    grid:Detach((i-1) * 2)
-                    grid:Detach((i-1) * 2)
-                    text:Destroy()
-                    remove_button:Destroy()
-                    scroller:update_scrollbars()
-                    break
-                end
-            end
-        end)
-    end
-
-    -- The add button
-    grid:AddSpacer(0)
-    local add_button = BmpButton(scroller.panel, wx.wxID_ANY, bmp.add)
-    grid:Add(add_button, 0, wx.wxALIGN_CENTER_VERTICAL)
-    add_button.ToolTip = wx.wxToolTip("Add field")
-    add_button:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function (evt)
-        scroller:Freeze()
-        add_field()
-        scroller:update_scrollbars()
-        scroller:Thaw()
-    end)
-
-    scroller.MinSize = wx.wxSize(-1, 100)
-
-    function panel:apply()
-        if not puzzle.fields then
-            puzzle.fields = {}
-        end
-        tablex.clear(puzzle.fields)
-        for _, ctrl in ipairs(fields) do
-            local value = ctrl.Value
-            if value ~= '' then
-                table.insert(puzzle.fields, value)
-            end
-        end
-        if #puzzle.fields == 0 then
-            puzzle.fields = nil
-        end
-    end
-
-    function panel:update_puzzle(p)
-        -- Clear the grid sizer
-        for i=0,#fields*2-1 do
-            local window = grid:GetItem(0).Window
-            grid:Detach(0)
-            window:Destroy()
-        end
-        tablex.clear(fields)
-        for _, name in ipairs(p.fields or {}) do
-            add_field(name)
-        end
-        scroller:update_scrollbars()
-    end
-
-    panel:update_puzzle(puzzle)
-
-    return panel
-end
-
-function get_download_function_panel(parent, puzzle)
-    local panel = collapse_panel(parent, "Custom Download Function")
-
-    local font = wx.wxFont(8, wx.wxFONTFAMILY_MODERN, wx.wxFONTSTYLE_NORMAL,
-                           wx.wxFONTWEIGHT_NORMAL, false)
-
-    local sizer = wx.wxBoxSizer(wx.wxVERTICAL)
-
-    local st = wx.wxStaticText(panel, wx.wxID_ANY, "function(puzzle)")
-    st.Font = font
-    sizer:Add(st, 0, wx.wxLEFT, 10)
-
-    local text = wx.wxTextCtrl(panel, wx.wxID_ANY, puzzle.func or '',
-        wx.wxDefaultPosition, wx.wxDefaultSize,
-        wx.wxTE_MULTILINE + wx.wxHSCROLL)
-    text.Font = font
-    text.MinSize = wx.wxSize(-1, 100)
-    sizer:Add(text, 1, wx.wxEXPAND + wx.wxLEFT, 20)
-
-    local st = wx.wxStaticText(panel, wx.wxID_ANY, "end")
-    st.Font = font
-    sizer:Add(st, 0, wx.wxLEFT, 10)
-
-    panel:set_panel(sizer)
-
-    function panel:apply()
-        local value = text.Value
-        if value:match('^%s*$') then
-            puzzle.func = nil
-        else
-            puzzle.func = value
-        end
-    end
-
-    function panel:update_puzzle(p)
-        text.Value = p.func or ''
-    end
-
-    return panel
-end
-
-function get_advanced_options_panel(parent, puzzle)
-    local panel = wx.wxPanel(parent, wx.wxID_ANY)
-
-    local sizer = wx.wxBoxSizer(wx.wxVERTICAL)
-    panel:SetSizer(sizer)
-
-    local grid = wx.wxFlexGridSizer(0, 2, 5, 5)
-    grid:AddGrowableCol(1)
-    sizer:Add(grid, 0, wx.wxEXPAND + wx.wxBOTTOM, 5)
-
-    local name = wx.wxTextCtrl(panel, wx.wxID_ANY, puzzle.name)
-    grid:Add(wx.wxStaticText(panel, wx.wxID_ANY, "Name:"), 0, wx.wxALIGN_CENTER_VERTICAL)
-    grid:Add(name, 1, wx.wxEXPAND)
-
-    local url = wx.wxTextCtrl(panel, wx.wxID_ANY, puzzle.url or '')
-    grid:Add(wx.wxStaticText(panel, wx.wxID_ANY, "URL:"), 0, wx.wxALIGN_CENTER_VERTICAL)
-    grid:Add(url, 1, wx.wxEXPAND)
-
-    local filename = wx.wxTextCtrl(panel, wx.wxID_ANY, puzzle.filename or '')
-    grid:Add(wx.wxStaticText(panel, wx.wxID_ANY, "Local filename:"), 0, wx.wxALIGN_CENTER_VERTICAL)
-    grid:Add(filename, 1, wx.wxEXPAND)
-
-    local directoryname = wx.wxTextCtrl(panel, wx.wxID_ANY, download.sanitize_name(puzzle.directoryname or puzzle.name))
-    grid:Add(wx.wxStaticText(panel, wx.wxID_ANY, "Subdirectory:"), 0, wx.wxALIGN_CENTER_VERTICAL)
-    grid:Add(directoryname, 1, wx.wxEXPAND)
-
-    local daysizer = wx.wxStaticBoxSizer(wx.wxHORIZONTAL, panel, "Days Available")
-    sizer:Add(daysizer, 0)
-    local days = {}
-    local daygrid = wx.wxGridSizer(1, 0, 5, 5)
-    daysizer:Add(daygrid, 1, wx.wxEXPAND + wx.wxALL, 5)
-    for i, label in ipairs({'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'}) do
-        local ctrl = wx.wxCheckBox(panel, wx.wxID_ANY, label)
-        ctrl.Value = puzzle.days[i]
-        table.insert(days, ctrl)
-        daygrid:Add(ctrl, 1, wx.wxEXPAND)
-    end
-
-    -- cURL options
-    local curlopts = get_curl_options_panel(panel, puzzle)
-    sizer:Add(curlopts, 0, wx.wxEXPAND + wx.wxTOP, 10)
-
-    -- fields
-    local fields = get_download_fields_panel(panel, puzzle)
-    sizer:Add(fields, 0, wx.wxEXPAND + wx.wxTOP, 10)
-
-    -- custom function
-    local func = get_download_function_panel(panel, puzzle)
-    sizer:Add(func, 0, wx.wxEXPAND + wx.wxTOP, 10)
-
-    function panel:apply()
-        puzzle.name = name.Value
-        puzzle.filename = filename.Value
-        puzzle.url = url.Value
-        if directoryname.Value ~= download.sanitize_name(puzzle.name) then
-            puzzle.directoryname = directoryname.Value
-        else
-            puzzle.directoryname = nil
-        end
-        for i, ctrl in ipairs(days) do
-            puzzle.days[i] = ctrl.Value
-        end
-        curlopts:apply()
-        fields:apply()
-        func:apply()
-    end
-
-    function panel:update_puzzle(p)
-        name.Value = p.name
-        filename.Value = p.filename
-        url.Value = p.url
-        directoryname.Value = p.directoryname or download.sanitize_name(p.name)
-        for i, ctrl in ipairs(days) do
-            ctrl.Value = p.days[i]
-        end
-        curlopts:update_puzzle(p)
-        fields:update_puzzle(p)
-        func:update_puzzle(p)
-    end
-
-    return panel
-end
+require 'download.config_gui'
 
 function show_advanced_options_dialog(parent, puzzle, title)
-    local dlg = wx.wxDialog(parent, wx.wxID_ANY, title or "Advanced Options")
+    local dlg = advanced_options_dialog(parent, title)
 
-    -- Create a new puzzle
+    -- Create a new puzzle if needed
     if puzzle == nil or not puzzle.name then
         puzzle = { name = '', filename = '', url = '',
                    days = { false, false, false, false, false, false, false} }
     end
 
-    -- Layout
-    local sizer = wx.wxBoxSizer(wx.wxVERTICAL)
-    dlg:SetSizer(sizer)
-    local panel = get_advanced_options_panel(dlg, puzzle)
-    panel.MinSize = wx.wxSize(400, -1)
-    sizer:Add(panel, 1, wx.wxEXPAND + wx.wxALL, 5)
-
-    local buttons = dlg:CreateButtonSizer(wx.wxOK + wx.wxCANCEL)
-    sizer:Add(buttons, 0, wx.wxALL, 5)
-
-    local reset_button = wx.wxButton(dlg, wx.wxID_ANY, "Reset to Defaults")
-    buttons:Add(reset_button)
-    reset_button:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function (evt)
+    -- Reset button event
+    dlg.reset_button:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED, function (evt)
         local p = download.get_default_puzzles()[puzzle.id]
         if p then
-            panel:update_puzzle(p)
+            dlg:set_puzzle(p)
         end
     end)
 
-    dlg:Fit()
-    dlg.MinSize = dlg.Size
-    dlg:Center()
+    -- Set puzzle values into the dialog
+    function dlg:set_puzzle(p)
+        local panel = dlg.panel
+        -- Basic options
+        panel.name.Value = p.name
+        panel.filename.Value = p.filename
+        panel.url.Value = p.url
+        panel.directoryname.Value = p.directoryname or download.sanitize_name(p.name)
+        for i, ctrl in ipairs(panel.days) do
+            ctrl.Value = p.days[i]
+        end
+        --cURL options
+        panel.curlopts:clear()
+        for k, v in pairs(p.curlopts or {}) do
+            if type(v) == 'string' then
+                panel.curlopts:add_option(k, v)
+            end
+        end
+        -- Custom fields
+        panel.fields:clear()
+        for _, name in ipairs(p.fields or {}) do
+            panel.fields:add_field(name)
+        end
+        -- Custom function
+        panel.custom_function.Value = p.func or ''
+    end
+
+    -- Transfer from the dialog to the puzzle
+    local function apply(p)
+        local panel = dlg.panel
+        -- Basic options
+        p.name = panel.name.Value
+        p.filename = panel.filename.Value
+        p.url = panel.url.Value
+        if panel.directoryname.Value ~= download.sanitize_name(p.name) then
+            p.directoryname = panel.directoryname.Value
+        else
+            p.directoryname = nil
+        end
+        for i, ctrl in ipairs(panel.days) do
+            p.days[i] = ctrl.Value
+        end
+        -- cURL options
+        if not p.curlopts then
+            p.curlopts = {}
+        end
+        tablex.clear(p.curlopts)
+        local opts_table = panel.curlopts.opts
+        for _, ctrls in ipairs(opts_table) do
+            local opt, value = unpack(ctrls)
+            -- Find the option as curl.OPT_[???]
+            opt = curl['OPT_' .. opt.StringSelection]
+            -- Set the option
+            p.curlopts[opt] = value.Value
+        end
+        if #opts_table == 0 then
+            p.curlopts = nil
+        end
+        -- Custom fields
+        if not p.fields then
+            p.fields = {}
+        end
+        tablex.clear(p.fields)
+        local fields_table = panel.fields.fields
+        for _, ctrl in ipairs(fields_table) do
+            local value = ctrl.Value
+            if value ~= '' then
+                table.insert(p.fields, value)
+            end
+        end
+        if #p.fields == 0 then
+            p.fields = nil
+        end
+        -- Custom function
+        local value = panel.custom_function.Value
+        if value:match('^%s*$') then
+            p.func = nil
+        else
+            p.func = value
+        end
+    end
+
+    dlg:set_puzzle(puzzle)
 
     if dlg:ShowModal() == wx.wxID_OK then
-        panel:apply()
+        apply(puzzle)
         dlg:Destroy()
         return puzzle
     end
@@ -1013,6 +696,8 @@ function download.get_config_panel(parent)
 
     return panel
 end
+
+xword.AddPreferencesPanel("Downloader", download.get_config_panel)
 
 function download.show_config_dialog(parent)
     xword.showerrors = false
