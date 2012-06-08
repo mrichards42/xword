231fc9c2a7ca2982e2e82957d9044d263b65e2a5
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2012-06-08 20:31:25 +0000

    Update Rex Parker blog script to find the current day's puzzle

--- trunk/scripts/blog/rexparker.lua
+++ trunk/scripts/blog/rexparker.lua
@@ -1,7 +1,4 @@
 local function rexparker(d)
-    -- We have to "search" for entries that are earlier than a given date, so
-    -- we need to search one past today.
-    d:adddays(1)
     blog.open("http://rexwordpuzzle.blogspot.com/search?updated-max=%Y-%m-%dT00:00:00-05:00&max-results=1", d)
 end
 
