d3db49b4c02a99c8d575f4b0eaa9bfadadd70928
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2012-07-19 02:14:22 +0000

    Bump puzzle downloader version

--- trunk/scripts/download/info.lua
+++ trunk/scripts/download/info.lua
@@ -1,7 +1,7 @@
 return {
     name = "Puzzle Downloader",
     packagename = "download",
-    version = "1.2",
+    version = "2.0",
     description = "Puzzle downloader and organizer.",
     requires = "0.6"
 }
