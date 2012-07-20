72e11e7e78d078f650b3573bf346a8b6b5137276
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2012-07-20 15:36:16 +0000

    Update info for a few scripts

--- trunk/scripts/download/info.lua
+++ trunk/scripts/download/info.lua
@@ -2,6 +2,20 @@ return {
     name = "Puzzle Downloader",
     packagename = "download",
     version = "2.0",
-    description = "Puzzle downloader and organizer.",
+    description = [[
+Puzzle downloader and organizer for most major puzzle sources including
+* NY Times
+* CrosSynergy
+* LA Times
+* Ink Well
+* The Onion
+* Matt Gaffney's Weekly Crossword Contest
+* Brendan Emmett Quigley
+
+Changes for version 2.0 (XWord 0.6):
+* NY Times Premium
+* New cleaner interface
+* Better configuration options
+]],
     requires = "0.6"
 }
