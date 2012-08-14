22023312f170f5476cf6feed4a88547a2fee4d5a
wackymorningdj <wackymorningdj@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2012-08-14 12:17:03 +0000

    Downloader version bump

--- trunk/scripts/download/info.lua
+++ trunk/scripts/download/info.lua
@@ -1,7 +1,7 @@
 return {
     name = "Puzzle Downloader",
     packagename = "download",
-    version = "2.1",
+    version = "2.2",
     description = [[
 Puzzle downloader and organizer for most major puzzle sources including
 * NY Times
@@ -12,7 +12,7 @@ Puzzle downloader and organizer for most major puzzle sources including
 * Matt Gaffney's Weekly Crossword Contest
 * Brendan Emmett Quigley
 
-Changes for version 2.1:
+Changes for version 2.2:
 * NY Times PDF
 * Other non-puzzle files can be downloaded (e.g. PDFs)
 
