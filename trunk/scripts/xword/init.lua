d546e18a6f2683fb9085275ace5b02a21e9b90e1
mrichards42 <mrichards42@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2013-12-11 05:54:10 +0000

    Add some documentation to xword.init

--- trunk/scripts/xword/init.lua
+++ trunk/scripts/xword/init.lua
@@ -1,15 +1,36 @@
--- ============================================================================
--- XWord additions
---     This package is loaded by XWord to initialize lua extensions and
---     add-ons
--- ============================================================================
+--- XWord for lua.
+-- This package is loaded by XWord to initialize lua extensions.
+-- @module xword
 
 require 'luapuz'
 
+-- table xword is already created in a C++ function
+
+--- Location of lua scripts.
+-- @field xword.scriptsdir
+
+--- Location of userdata.
+-- Config files are stored in a userdata subdir.
+-- @field xword.userdatadir
+
+--- Location of config files.
+-- @field xword.configdir
+
+--- Location of application images.
+-- @field xword.imagesdir
+
+--- Is XWord being run in portable mode?
+-- All paths are different in portable mode.
+-- @field xword.isportable
+
+--- XWord's main wxFrame.
 xword.frame = xword.GetFrame()
 
 -- Cleanup functions
 xword.cleanup = {}
+
+--- Add functions to be called as XWord is closing.
+-- @param func A function to be called on cleanup.
 function xword.OnCleanup(func)
     table.insert(xword.cleanup, func)
 end
