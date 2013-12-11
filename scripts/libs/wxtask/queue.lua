612c027ee5562b8cf00b6f81e9d4adf64d47cb95
mrichards42 <mrichards42@ee7b44e0-4cd8-42ac-948d-ba64932cbd56>
2013-12-11 05:40:58 +0000

    Rewrote the lua half of luatask. Renamed the c luatask library to c-task so require 'task' loads the lua module. Hooks into wxApp's idle event for a custom event loop. Task object: methods include post, connect/disconnect events

--- scripts/libs/wxtask/queue.lua
+++ scripts/libs/wxtask/queue.lua
@@ -1,55 +1,71 @@
--- Adapting the queue example from PiL as a class
+-- A double-ended queue
 
 local Queue = {}
+Queue.__index = Queue
 
-function Queue:new()
-    local obj = {first = 0, last = -1}
-    self.__index = self
-    setmetatable(obj, self)
+function Queue.new()
+    local obj = {_front = 1, _back = 0}
+    setmetatable(obj, Queue)
     return obj
 end
 
-function Queue:push(value)
-  local first = self.first - 1
-  self.first = first
-  self[first] = value
+function Queue:front()
+    return self[self._front]
+end
+
+function Queue:back()
+    return self[self._back]
+end
+
+function Queue:push(v)
+    self._back = self._back + 1
+    self[self._back] = v
 end
 
 function Queue:pop()
-  local last = self.last
-  if self.first > last then return nil end -- queue is empty
-  local value = self[last]
-  self[last] = nil -- allow garbage collection
-  self.last = last - 1
-  return value
+    if self:empty() then return end
+    local value = self:front()
+    self[self._front] = nil
+    self._front = self._front + 1
+    return value
 end
 
-function Queue:get_last()
-    return self[self.last]
+function Queue:pushfront(v)
+    self._front = self._front - 1
+    self[self._front] = v
+end
+
+function Queue:popback()
+    if self:empty() then return end
+    local value = self:back()
+    self[self._back] = nil
+    self._back = self._back - 1
+    return value
 end
 
 function Queue:iter()
-    local current = self.last + 1
+    local i = self._front - 1
     return function()
-        current = current - 1
-        return self[current]
+        i = i + 1
+        return self[i]
     end
 end
 
 function Queue:clear()
-  while self.first <= self.last do
-    self[self.last] = nil -- garbage collect this value
-    self.last = self.last - 1
-  end
-  self.first = 0
-  self.last = -1
+    for i=self._first,self._last do
+        self[i] = nil
+    end
+    self._front = 1
+    self._back = 0
 end
 
--- Using __len doesn't override the # operator for tables
+-- NB: __len doesn't override the # operator for tables until 5.2
 function Queue:length()
-    return self.last - self.first + 1
+    return self._back - self._front + 1
 end
 
-function task.newQueue()
-    return Queue:new()
+function Queue:empty()
+    return self._front > self._back
 end
+
+return Queue.new
