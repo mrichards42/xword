69b51f0937c1d3a7d11383a955b0692ca685cedf
Jeff Davidson <jpd@google.com>
2016-10-09 20:53:59 -0700

    Configure Travis and AppVeyor builds.
    This is a start towards resolving Issue #40. These configs build the
    debug and release builds of XWord on Mac and Windows. If a new release
    is created on GitHub, a release build will be zipped up and posted as
    part of the release.
    
    The --enable-unicode reference in README.md is removed, as this is the
    default as of wxWidgets 2.9.

---
+++ build-wxwidgets.sh
@@ -0,0 +1,54 @@
+#!/bin/sh
+# Script to build and install wxWidgets, with our custom tweaks patch.
+
+set -e
+
+CONFIGURATION=$1
+
+INSTALL_PATH="$HOME/wxWidgets"
+mkdir -p $INSTALL_PATH
+
+WX_CONFIGURE_FLAGS="\
+  --disable-shared \
+  --enable-compat28 \
+  --enable-universal-binary=i386 \
+  --with-macosx-version-min=10.6 \
+  CFLAGS=-fvisibility-inlines-hidden \
+  CXXFLAGS=-fvisibility-inlines-hidden \
+  CPPFLAGS=-fvisibility-inlines-hidden \
+  OBJCFLAGS=-fvisibility-inlines-hidden \
+  OBJCXXFLAGS=-fvisibility-inlines-hidden"
+WX_MAKE_FLAGS="SHARED=0"
+
+if [ "$CONFIGURATION" = "Debug" ]; then
+  WX_CONFIGURE_FLAGS="$WX_CONFIGURE_FLAGS --enable-debug"
+  WX_MAKE_FLAGS="$WX_MAKE_FLAGS BUILD=debug"
+elif [ "$CONFIGURATION" = "Release" ]; then
+  WX_MAKE_FLAGS="$WX_MAKE_FLAGS BUILD=release"
+else
+  echo "Unsupported configuration: $CONFIGURATION"
+  echo "Please use one of Debug or Release"
+  exit 1
+fi
+echo "Building wxWidgets for configuration: $CONFIGURATION"
+
+if [ "$OSTYPE" = "darwin"* ]; then
+  # Mac OS X
+  BUILD_COMMAND="./configure --prefix=$INSTALL_PATH $WX_CONFIGURE_FLAGS && make && make install"
+else
+  # Windows
+  BUILD_COMMAND="sed -e 's/WXWIN_COMPATIBILITY_2_8 0/WXWIN_COMPATIBILITY_2_8 1/' -i include/wx/msw/setup.h"
+  BUILD_COMMAND="$BUILD_COMMAND && cd build/msw && nmake.exe -f makefile.vc $WX_MAKE_FLAGS"
+  BUILD_COMMAND="$BUILD_COMMAND && cp -a ../../lib ../../include $INSTALL_PATH"
+fi
+
+# Check to see if the cache directory is empty
+if [ ! -d "$INSTALL_PATH/lib" ]; then
+  curl -fsSL -o wxWidgets-3.1.0.tar.bz2 https://github.com/wxWidgets/wxWidgets/releases/download/v3.1.0/wxWidgets-3.1.0.tar.bz2
+  tar -xjf wxWidgets-3.1.0.tar.bz2
+  cd wxWidgets-3.1.0
+  patch -p1 -i ../wxaui-tweaks.patch
+  eval $BUILD_COMMAND
+else
+  echo "Using cached directory."
+fi
