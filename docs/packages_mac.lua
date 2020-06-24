6e7bce3fd84953522e05a3c5a1e479726b338df7
Mike Richards <mrichards42@gmx.com>
2020-06-24 18:31:18 -0400

    Add packages files for updater

---
+++ docs/packages_mac.lua
@@ -0,0 +1,7 @@
+return {
+  xword = {
+    download = "https://github.com/mrichards42/xword/releases/download/0.7.0/XWord-macOS.zip",
+    name = "XWord",
+    version = "0.7.0",
+  },
+}
---
+++ docs/packages_windows.lua
@@ -0,0 +1,7 @@
+return {
+  xword = {
+    download = "https://github.com/mrichards42/xword/releases/download/0.7.0/XWord-Windows.exe",
+    name = "XWord",
+    version = "0.7.0",
+  },
+}
