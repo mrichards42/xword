7520d499bfa60819c1ddf53f17803feba9c6f75a
Jeff Davidson <jpd@google.com>
2022-07-28 14:45:12 -0700

    Stop escaping metadata when loading .puz files.
    This is now handled by default, since is_html isn't specified.

--- puz/formats/puz/load_puz.cpp
+++ puz/formats/puz/load_puz.cpp
@@ -123,9 +123,9 @@ void LoadPuz(Puzzle * puz, const std::string & filename, void * /* dummy */)
     puz->NumberGrid();
 
     // General puzzle info
-    puz->SetTitle(escape_xml(decode_text(f.ReadString())));
-    puz->SetAuthor(escape_xml(decode_text(f.ReadString())));
-    puz->SetCopyright(escape_xml(decode_text(f.ReadString())));
+    puz->SetTitle(decode_text(f.ReadString()));
+    puz->SetAuthor(decode_text(f.ReadString()));
+    puz->SetCopyright(decode_text(f.ReadString()));
 
     // Clues
     std::vector<string_t> clues;
@@ -143,7 +143,7 @@ void LoadPuz(Puzzle * puz, const std::string & filename, void * /* dummy */)
 
     // Notes
     std::string notes = f.ReadString();
-    puz->SetNotes(escape_xml(decode_text(notes)));
+    puz->SetNotes(decode_text(notes));
 
     puz->SetOk(true);
 
